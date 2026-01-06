fff98ac8a430e71eddbfc95680839bb602b2951d
Manish2928 <manishkashyap9611@gmail.com>
2026-01-06 08:26:52 +0530

    Hyperparameter Traianing

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,12 +48,57 @@ class ModelTrainer:
                 "Linear regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                # "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
 
             }
 
-            model_reprot:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test, models=models)
+            params = {
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "Gradient Boosting":{
+                    
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+
+                },
+
+                "Linear regression":{},
+
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11]
+
+                },
+
+                "XGBRegressor":{
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate':[.1,.01,.05,.001],
+
+                },
+
+                # "CatBoosting Regressor":{
+                #     'depth':[6,8,10],
+                #     'iterations': [30,50,100],
+                #     'learning_rate':[0.01,0.05,0.1],
+
+                # },
+
+                "AdaBoost Regressor":{
+                    'n_estimators': [8,16,32,64,128,256],
+                    'learning_rate':[0.01,0.05,0.1],
+
+                },
+            }
+
+            
+            model_reprot:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test, models=models,params=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_reprot.values()))
